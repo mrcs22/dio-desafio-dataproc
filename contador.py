01d122cf8b50acf744f944c71c3234a9824b4494
mrcs22 <marcusmoraes2010@hotmail.com>
2022-04-16 02:26:57 -0300

    refactor: extract bucket name to variable

--- contador.py
+++ contador.py
@@ -1,7 +1,12 @@
 import sys
 from pyspark import SparkContext, SparkConf
+
+BUCKET_NAME = "desafio-dataproc-22"
+
 if __name__ == "__main__":
-    sc = SparkContext("local","PySpark Exemplo - Desafio Dataproc")
-    words = sc.textFile("gs://{SEU_BUCKET}/livro.txt").flatMap(lambda line: line.split(" "))
-    wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b).sortBy(lambda a:a[1], ascending=False)
-    wordCounts.saveAsTextFile("gs://{SEU_BUCKET}/resultado")
+    sc = SparkContext("local", "PySpark Exemplo - Desafio Dataproc")
+    words = sc.textFile(
+        f"gs://{BUCKET_NAME}/livro.txt").flatMap(lambda line: line.split(" "))
+    wordCounts = words.map(lambda word: (word, 1)).reduceByKey(
+        lambda a, b: a + b).sortBy(lambda a: a[1], ascending=False)
+    wordCounts.saveAsTextFile(f"gs://{BUCKET_NAME}/resultado")
